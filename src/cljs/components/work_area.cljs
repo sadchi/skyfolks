09a9ea61399bf9f1e62213e14938a4c6e24209c8
ahuba <ahuba@aligntech.com>
2018-01-31 19:40:38 +0300

    ref

--- src/cljs/components/work_area.cljs
+++ src/cljs/components/work_area.cljs
@@ -14,6 +14,8 @@
   {
    :accent  "#607D8B"
    :padding 16
+   :height  16
+   :width   16
    })
 
 (def params (merge default-params (get p/params (name (namespace ::x)))))
@@ -21,16 +23,45 @@
 (defn v [& k] (get-in params k))
 
 
-(def work-area-s ^:css {:position "absolute"
-                        :content  "\" \""
-                        :left     0
-                        :right    0
-                        :top      0
-                        :bottom   0
-                        :overflow "scroll"})
+(def work-area-s ^:css [cs/flex-box
+                        {:position        "absolute"
+                         :align-items     "center"
+                         :justify-content "center"
+                         :content         "\" \""
+                         :left            0
+                         :right           0
+                         :top             0
+                         :bottom          0
+                         :overflow        "scroll"}])
+
+
+(def work-area__block ^:css {})
+
+(def work-area__row ^:css {:height        (px (v :height))
+                           :margin-bottom (px -1)
+                           })
+
+(def work-area__row__cell ^:css {:margin-right (px -1)
+                                 :display      "inline-block"
+                                 :width        (px (v :width))
+                                 :height       "100%"
+                                 :background   "grey"
+
+                                 :border       "1px solid blue"})
 
 (defn work-area [data]
   (fn [data]
-    [:div (c/cls 'work-area-s) @data]))
+    [:div (c/cls 'work-area-s)
+     [:div (c/cls 'work-area__block)
+      [:div (c/cls 'work-area__row)
+       [:div (c/cls 'work-area__row__cell)]
+       [:div (c/cls 'work-area__row__cell)]
+
+       ]
+      [:div (c/cls 'work-area__row)
+       [:div (c/cls 'work-area__row__cell)]
+       [:div (c/cls 'work-area__row__cell)]
+       [:div (c/cls 'work-area__row__cell)]
+       ]]]))
 
 (c/add-css (ns-interns 'components.work-area))
