e0e99fb1222b9d111929e155ae4ee8eca1d33c5c
ahuba <ahuba@aligntech.com>
2018-02-14 19:49:26 +0300

    ref

--- src/clj/skyfolks/world.clj
+++ src/clj/skyfolks/world.clj
@@ -4,17 +4,21 @@
     [ring.util.response :as r :refer [created file-response response not-found]]
     ))
 
+(def our-world (agent nil))
+
 (defn cur-world [req])
-
-
 
 (defn save-world [cfg req]
   (log/debug "save-world issued " req)
   (let [{:keys [world filename]} (:params req)]
+    (send our-world (fn [_] world))
     (spit (str (get-in cfg [:world :location]) "/" filename) world)
     (response {:ok true})))
 
 (defn load-world [cfg req]
   (log/debug "load-world issued " req)
-  (response {:ok true}))
+  (let [filename (get-in req [:params :filename])]
+    (if-not filename
+      (response @our-world)
+      (response (read-string (slurp (str (get-in cfg [:world :location]) "/" filename)))))))
 
